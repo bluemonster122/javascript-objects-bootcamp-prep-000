99d27d93f24a91eefb088d54a1007176e1d4d4ce
L H Gilfoyle <l.gilfoyle@wlv.ac.uk>
2018-07-06 13:58:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,5 @@
 // Limits playlist to only have 1 song per artist
 var playlist = {}
 
-function updatePlaylist(playlist, artist, song) {
-  return playlist[artist] = song;
-}
-
-function removeFromPlaylist(playlist, artist) {
-  delete playlist.artist
-  return playlist;
-}
+function updatePlaylist(playlist, artist, song) { return Object.assign({} playlist, {artist: song}) }
+function removeFromPlaylist(playlist, artist) {  delete playlist.artist; return playlist; }
